31aa9492310074db31319f62ff064a79309a78ff
FaruqAlfa <alfafahmi172@gmail.com>
2023-02-14 15:25:54 +0700

    name changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,6 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">Mukhamad Faruq Al Fahmi</h1></div>
+            <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">M. Faruq Al Fahmi</h1></div>
     </body>
 </html>
